d6659c3ae7f7c0bd61235a17424efd0a8383a6af
Albert Putra Purnama <albertputrapurnama@gmail.com>
2017-03-28 16:38:05 -0700

    Elevator L8 first commit

---
+++ Elevator/Rider.cpp
@@ -0,0 +1,24 @@
+//Programmer: Albert Putra Purnama
+//Student ID: 1559220
+
+#include "Rider.h"
+
+#include<iostream>
+using namespace std;
+
+Rider::Rider(int a, int b)
+:from(a) , to(b)
+{
+}
+
+Rider& Rider::operator=(const Rider& r)
+{
+  Rider& host = *this;
+  if (this != &r)
+  {
+    host.from = r.from;
+    host.to = r.to;
+  }
+
+  return host;
+}
---
+++ Elevator/Rider.h
@@ -0,0 +1,20 @@
+//Programmer: Albert Putra Purnama
+//Student ID: 1559220
+
+#ifndef Rider_h
+#define Rider_h
+
+#include<iostream>
+using namespace std;
+
+#include<cstring>
+
+struct Rider
+{
+  int to;
+  int from;
+  Rider(int, int);
+  Rider& operator=(const Rider&);
+};
+
+#endif
